6da96b6ec3c7ec5ec9cc8e0699f1b5c18e7b556f
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2022-05-17 12:04:55 -0400

    Refactor n4 bsub script

--- bsub/bsub_n4.sh
+++ bsub/bsub_n4.sh
@@ -1,11 +1,12 @@
 # N4 correction
 module load ANTs
 
-IMAGES=($(cut -d, -f5 file_list.csv))
-
-GADG_IMAGES=($(printf -- '%s\n' "${IMAGES[@]}" | grep gadgetron))
-
-for image in ${GADG_IMAGES[@]}
-do 
-    bsub -J $image -o logs -e logs N4BiasFieldCorrection -d 3 -i ${image} -o "$(dirname ${image})/$(basename ${image} .nii.gz)_n4.nii.gz"
+for image in $(ls /project/mscamras/gadgetron/datasets-new/*/*/*.nii.gz); do 
+    
+    out_image=$(dirname $image)/n4/$(basename $image .nii.gz)_n4.nii.gz
+    
+    if [ ! -e "$out_image" ]; then
+        mkdir -p $(dirname $image)/n4
+        bsub -J "$image" -o logs/n4.log -e logs/n4.log N4BiasFieldCorrection -d 3 -i "$image" -o "$out_image"
+    fi
 done
