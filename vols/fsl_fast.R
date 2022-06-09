529e5a6719704060bc27097d5550d993785f5a39
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2022-06-09 13:58:05 -0400

    Refactor FAST

--- vols/fsl_fast.R
+++ vols/fsl_fast.R
@@ -7,16 +7,12 @@ library(parallel)
 
 ##FSL FAST segmentation for GM, WM, and CSF
 
-t1s = system(paste("find /project/mscamras/gadgetron/datasets-new -name *brain_n4.nii.gz | grep 'MPRAGE'"),intern=TRUE) 
+argv = commandArgs(trailingOnly = TRUE)
+t1 = argv[1]
+out_dir = argv[2]
+out_dir = file.path(out_dir, "FAST")
+dir.create(out_dir)
 
-do.fast = function(i){
-
-    output.dir_path = file.path(dirname(t1s[i]), "FAST")
-    dir.create(output.dir_path)
-
-    MPRAGE_fast = fast(file = t1s[i],
-                        outfile = file.path(output.dir_path, basename(nii.stub(t1s[i]))),
-                        bias_correct = FALSE)
-}
-
-mclapply(1:length(t1s), do.fast, mc.cores = Sys.getenv("LSB_DJOB_NUMPROC"))
+out_file = file.path(out_dir, neurobase::nii.stub(t1, bn = TRUE))
+t1_fast = fslr::fast(t1, outfile = out_file, bias_correct = FALSE)
+message(sprintf("FAST output saved as %s", out_file))
