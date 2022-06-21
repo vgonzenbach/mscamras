6fe1f02ecbf0d3ef07c84ad4faea97a3b417de03
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2022-06-21 10:05:57 -0400

    Simplify flair to t1 registration

--- preproc/reg_flair2mprage.R
+++ preproc/reg_flair2mprage.R
@@ -5,31 +5,28 @@ library(fslr)
 library(stringr)
 
 #' Registers flair to mprage
-if (!exists(argv)){
-    argv = commandArgs(trailingOnly = TRUE)
-    flair2map = argv[1]
-    mprage_path = argv[2]
-    flair2transform = argv[3]
-}
+argv = commandArgs(trailingOnly = TRUE)
+flair = argv[1]
+mprage = argv[2]
 
 
 print('Computing transformation...')
-flair2mprage = antsRegistration(fixed = extrantsr::oro2ants(neurobase::readnii(mprage_path)), 
-                                moving = extrantsr::oro2ants(neurobase::readnii(flair2map)),
+flair2mprage = antsRegistration(fixed = extrantsr::oro2ants(neurobase::readnii(mprage)), 
+                                moving = extrantsr::oro2ants(neurobase::readnii(flair)),
                                 typeofTransform = "Rigid",
                                 verbose = TRUE)
 
 print('Applying transformation...')                            
-flair_reg = antsApplyTransforms(fixed=extrantsr::oro2ants(neurobase::readnii(mprage_path)), 
-                                moving=extrantsr::oro2ants(neurobase::readnii(flair2transform)),
+flair_reg = antsApplyTransforms(fixed=extrantsr::oro2ants(neurobase::readnii(mprage)), 
+                                moving=extrantsr::oro2ants(neurobase::readnii(flair)),
                                 transformlist = flair2mprage$fwdtransforms,
                                 interpolator ="WelchWindowedSinc",
                                 verbose = TRUE)
 
 print('Saving transformed image...')
 
-outdir = file.path(dirname(flair2transform), "reg")
+outdir = file.path(dirname(flair), "reg")
 dir.create(outdir)
-outpath = file.path(outdir, stringr::str_replace(basename(flair2transform), '.nii.gz', '_reg.nii.gz'))
+outpath = file.path(outdir, stringr::str_replace(basename(flair), '.nii.gz', '_reg.nii.gz'))
 antsImageWrite(flair_reg, outpath)
 print('Registration complete.')
