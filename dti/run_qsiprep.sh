b4dfdb8788f59d121aa69a901f35afe97aadd426
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2022-08-29 13:16:35 -0400

    Change output path of qsiprep

--- dti/run_qsiprep.sh
+++ dti/run_qsiprep.sh
@@ -1,5 +1,5 @@
 cd $(dirname $0)/..
 NCORES=32
 bsub -n $NCORES -o logs/qsiprep.log -e logs/qsiprep.log singularity run --cleanenv -B /project/mscamras -B /home/vgonzenb/mscamras -B /appl/freesurfer-7.1.1/license.txt:/opt/freesurfer/license.txt \
-    /project/singularity_images/qsiprep_0.16.0RC3.sif /home/vgonzenb/mscamras/data /home/vgonzenb/mscamras/dti participant \
+    /project/singularity_images/qsiprep_0.16.0RC3.sif /home/vgonzenb/mscamras/data /home/vgonzenb/mscamras/data participant \
     --output-resolution 2.2 --fs-license-file /opt/freesurfer/license.txt --separate-all-dwis --nthreads $NCORES
