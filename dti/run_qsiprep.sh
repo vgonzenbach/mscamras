f67ca645e511db2316d72ae04466b268a1db58a6
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2022-09-19 13:35:46 -0400

    Run qsiprep by subject

--- dti/run_qsiprep.sh
+++ dti/run_qsiprep.sh
@@ -1,8 +1,9 @@
 cd $(dirname $0)/..
-NCORES=32
-bsub -J qsiprep -n $NCORES -o logs/qsiprep.log -e logs/qsiprep.log singularity run --cleanenv -B /project/mscamras -B /home/vgonzenb/mscamras -B /appl/freesurfer-7.1.1/license.txt:/opt/freesurfer/license.txt \
-    /project/singularity_images/qsiprep_0.16.0RC3.sif /home/vgonzenb/mscamras/data /home/vgonzenb/mscamras/data/derivatives participant \
-    --output-resolution 2.2 \
-    --fs-license-file /opt/freesurfer/license.txt \
-    --distortion-group-merge average \
-    --nthreads $NCORES
+for subject in $(python -c "import sys; from bids import BIDSLayout; layout = BIDSLayout('data'); subjects = layout.get_subjects(); sys.stdout.write(' '.join(subjects))"); do
+    bsub -J qsiprep -o logs/qsiprep/sub-$subject.log -e logs/qsiprep/sub-$subject.log singularity run --cleanenv -B /project/mscamras -B /home/vgonzenb/mscamras -B /appl/freesurfer-7.1.1/license.txt:/opt/freesurfer/license.txt \
+        /project/singularity_images/qsiprep_0.16.0RC3.sif /home/vgonzenb/mscamras/data /home/vgonzenb/mscamras/data/derivatives participant \
+        --participant-label $subject \
+        --output-resolution 2.2 \
+        --fs-license-file /opt/freesurfer/license.txt \
+        --distortion-group-merge average
+done
