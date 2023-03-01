142098987767a01ecf5e11d47ea5f2b6f9e9318c
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2023-03-01 09:40:28 -0500

    do not fix json in this script

--- inv/check_json_dti.py
+++ inv/check_json_dti.py
@@ -1,3 +1,4 @@
+#%% set up
 import os
 import sys
 import json
@@ -25,25 +26,7 @@ df = pd.DataFrame.from_records(img_info)
 df.columns = ['subj', 'site', 'dir', 'visit', 'PhaseEncodingDirection', 'PhaseEncodingSteps']
 #df.to_csv(sys.stdout, index=False)
 
+# %%
 # Hopkins data is missing 'PhaseEncodingDirection' attribute"
 # NIH data shows discrepancy in PhaseEncodingDirection was assigned
 df.pivot_table(columns='PhaseEncodingDirection', index=['site', 'dir'], aggfunc='size')
-
-# Fix the direction in Hopkins
-# TODO: generalize to all .json files not only Hopkins ?
-for fn in dwi_filenames:
-    if 'Hopkins' in fn:
-        with open(fn, 'r') as f:
-            hopkins_json = json.load(f)
-        
-        if 'PhaseEncodingDirection' not in hopkins_json.keys():
-            if 'AX' in fn: # assign direction according to dir field
-                hopkins_json['PhaseEncodingDirection'] = 'j'
-            elif 'XPhase' in fn:
-                hopkins_json['PhaseEncodingDirection'] = 'j-'
-        
-        if 'TotalReadoutTime' not in hopkins_json.keys():
-            hopkins_json['TotalReadoutTime'] = 0.031185
-            
-        with open(fn, 'w') as f:
-            json.dump(hopkins_json, f, indent=1)
