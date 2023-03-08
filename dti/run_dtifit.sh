7d8e4141a55513b6abd37c91be78784e000d7803
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2023-03-08 10:19:46 -0500

    calculate AD and RD maps

--- dti/run_dtifit.sh
+++ dti/run_dtifit.sh
@@ -3,25 +3,27 @@
 module load fsl
 cd $(dirname $0)/..
 
-data_version=v4
+data_version=v5
 
 for dwi in $(find data/${data_version}/derivatives/qsiprep -path '**preproc_dwi.nii.gz'); do
     mask=$(echo $dwi | sed 's/preproc_dwi/brain_mask/g')
     bval=${dwi%%.*}.bval # use parameter expansion to reassign extension
     bvec=${dwi%%.*}.bvec
-    cni=$(echo ${dwi%%.*} | sed 's/space-T1w_desc-preproc_dwi/confounds/g').tsv
+
 
     sub=$(echo $dwi | grep -Eo sub-[A-Za-z0-9]+ | grep -Eo [0-9]+ | head -n1)
    
-    out=data/${data_version}/derivatives/dtifit/sub-$sub/dwi/$(basename $dwi _dwi.nii.gz)
+    out=data/${data_version}/derivatives/dtifit/sub-$sub/dwi/$(basename $dwi .nii.gz)
     mkdir -p $(dirname $out)
 
-    bsub -J dtifit_$sub -oo logs/dtifit//${data_version}/$sub.log -eo logs/dtifit/${data_version}/$sub.log dtifit --data=$dwi \
+    bsub -J dtifit_$sub -oo logs/dtifit/${data_version}/$sub.log -eo logs/dtifit/${data_version}/$sub.log dtifit --data=$dwi \
         --out=$out \
         --mask=$mask \
         --bvals=$bval \
         --bvecs=$bvec 
-    bsub -w dtifit_$sub -ti 
+    # copy file with AD name 
+    bsub -w dtifit_$sub -ti cp ${out}_L1.nii.gz ${out}_AD.nii.gz
+    bsub -w dtifit_$sub -ti fslmaths ${out}_L2.nii.gz -add ${out}_L3.nii.gz -div 2 ${out}_RD.nii.gz
 done
 
 echo '{
@@ -30,4 +32,4 @@ echo '{
     "PipelineDescription": {
         "Name": "FSL DTIFIT"
     }
-}' > data/derivatives/dtifit/dataset_description.json
+}' > data/${data_version}/derivatives/dtifit/dataset_description.json
