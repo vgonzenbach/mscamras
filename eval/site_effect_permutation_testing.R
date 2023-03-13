5370029616226968328d7782f6482b059620f16f
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2023-03-13 18:27:33 -0400

    edit permutation script for dti data

--- eval/site_effect_permutation_testing.R
+++ eval/site_effect_permutation_testing.R
@@ -3,10 +3,11 @@ set.seed(1979)
 
 setwd(rprojroot::find_rstudio_root_file())
 
-argv = commandArgs(trailingOnly = TRUE)
-vol_df = read.csv(argv[1])
+argv <- commandArgs(trailingOnly = TRUE)
+seg_df = read.csv(argv[1], 
+                  colClasses = c("subject" = "character", "visit" = "character"))
 
-get_ratio_stat = function(vol_df){
+get_ratio_stat = function(seg_df){
   
   get_mean_intersite_diff = function(subj_df) {
     #' Calculates mean inter-site difference for a "subjects" data.frame
@@ -27,8 +28,8 @@ get_ratio_stat = function(vol_df){
   }
   
   # get intrasite differences
-  mean_intrasite_diffs = vol_df %>% 
-    split(list(vol_df$ID, vol_df$site)) %>% # split dataset by subject and site, i.e. generate list of data.frames
+  mean_intrasite_diffs = seg_df %>% 
+    split(list(seg_df$subject, seg_df$site)) %>% # split dataset by subject and site, i.e. generate list of data.frames
     lapply(function (x) select(x, where(is.numeric))) %>% # select volumes (in each df)
     lapply(function (x) sapply(x, function(x) abs(diff(x)))) %>% # take absolute value of row-wise difference in all columns
     bind_rows (.id = "subj-site") %>% # put all data.frame elements of the list into a single data.frame
@@ -36,8 +37,8 @@ get_ratio_stat = function(vol_df){
     colMeans(na.rm = TRUE) %>% t() %>% data.frame()
   
   # get intersite differences
-  mean_intersite_diffs = vol_df %>% 
-    split (vol_df$ID) %>% 
+  mean_intersite_diffs = seg_df %>% 
+    split (seg_df$subject) %>% 
     lapply(get_mean_intersite_diff) %>% 
     bind_rows() %>% 
     colMeans(na.rm = TRUE) %>% t() %>% data.frame()
@@ -46,35 +47,22 @@ get_ratio_stat = function(vol_df){
   return(diff_ratio)
 }
 
-permute_ratio_stat = function(vol_df, n.perms = 10000){
+permute_ratio_stat = function(seg_df, n.perms = 10000){
   perm_ratio_stats = list()
   
   for (n.perm in 1:n.perms){
-    perm_df = select(vol_df, ID, site, img_type, img_source)
+    perm_df = select(seg_df, subject, site, visit)
     perm_ind = sample(1:nrow(perm_df), nrow(perm_df), replace = FALSE)
     perm_df = cbind(perm_df, 
-                    select(vol_df[perm_ind, ], where(is.numeric)))
+                    select(seg_df[perm_ind, ], where(is.numeric)))
     perm_ratio_stats = c(perm_ratio_stats, list(get_ratio_stat(perm_df)))
   }
   
   return(perm_ratio_stats %>% bind_rows())
 }
 
-test_ratio_stat = function(ratio.stats, null.dists){
-  p.vals = c()
-  for (col in colnames(ratio.stat)){
-    ratio.stat = ratio.stats[, col]
-    null.dist = null.dists[, col]
-    
-    percentile = ecdf(null.dist)
-    p.vals = c(p.vals, percentile(ratio.stat))
-  }
-  names(p.vals) = colnames(ratio.stats)
-  return(p.vals)
-}
-
-ratio.stat = get_ratio_stat(vol_df)
-null.dists = permute_ratio_stat(vol_df, 10000)
+ratio.stat = get_ratio_stat(seg_df)
+null.dists = permute_ratio_stat(seg_df)
 
 saveRDS(ratio.stat, sprintf("results/%s_ratio_stat.rds", tools::file_path_sans_ext(basename(argv[1]))))
 saveRDS(null.dists, sprintf("results/%s_null.rds", tools::file_path_sans_ext(basename(argv[1]))))
