af7b2e425b5ef9a32391fcdcf6aedb6b756af21a
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2023-03-15 12:44:17 -0400

    correct coments

--- bsub/bsub_etl.sh
+++ bsub/bsub_etl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $(dirname $0)/..
 
-# resample dtifit results to fit segmentation masks
+# extract and transform data
 bsub -J extract -n 500 -oo logs/etl.log -eo logs/etl.log Rscript seg/extract_data.R 
 bsub -J transform -w extract -ti -o logs/etl.log -e logs/etl.log Rscript seg/transform_data.R 
